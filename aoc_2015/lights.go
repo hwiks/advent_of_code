671686358f78b77d0dd47394111f5e10b27ae4cc
Anna Lanteri <annalanterip@gmail.com>
2025-10-10 20:02:07 +0200

    Solves riddle 6.1 and waters my crops, moisturizes my skin

--- aoc_2015/lights.go
+++ aoc_2015/lights.go
@@ -7,27 +7,6 @@ import (
 	"strings"
 )
 
-func turn_on(lights [1000000][3]int, edges []int) [1000000][3]int {
-	var first = edges[:2]
-	var last = edges[2:]
-
-	for i := range lights {
-		if light_in_range(lights[i], first, last) {
-			lights[i][2] = 1
-		}
-	}
-	return lights
-
-}
-
-func light_in_range(light [3]int, first []int, last []int) bool {
-
-	var x int = light[0]
-	var y int = light[1]
-
-	return (x >= first[0]) && (x <= last[0]) && (y >= first[1]) && (y <= last[1])
-}
-
 func get_edges(instruction string) []int {
 	re := regexp.MustCompile("[0-9]+")
 	var edges_string []string = re.FindAllString(instruction, -1)
@@ -40,33 +19,50 @@ func get_edges(instruction string) []int {
 	return edges
 }
 
-func turn_off(lights [1000000][3]int, edges []int) [1000000][3]int {
+func turn_on(lights [1000][1000]int, edges []int) [1000][1000]int {
 	var first = edges[:2]
 	var last = edges[2:]
 
-	for i := range lights {
-		if light_in_range(lights[i], first, last) {
-			lights[i][2] = 0
+	for i := first[0]; i <= last[0]; i++ {
+		for j := first[1]; j <= last[1]; j++ {
+			lights[i][j] = 1
 		}
+
 	}
+
+	return lights
+
+}
+
+func turn_off(lights [1000][1000]int, edges []int) [1000][1000]int {
+	var first = edges[:2]
+	var last = edges[2:]
+
+	for i := first[0]; i <= last[0]; i++ {
+		for j := first[1]; j <= last[1]; j++ {
+			lights[i][j] = 0
+		}
+
+	}
+
 	return lights
 }
 
-func toggle(lights [1000000][3]int, edges []int) [1000000][3]int {
+func toggle(lights [1000][1000]int, edges []int) [1000][1000]int {
 	var first = edges[:2]
 	var last = edges[2:]
 
-	for i := range lights {
-		if light_in_range(lights[i], first, last) {
-
-			fmt.Println("light ", lights[i])
-			if lights[i][2] == 0 {
-				lights[i][2] = 1
-			} else if lights[i][2] == 1 {
-				lights[i][2] = 0
+	for i := first[0]; i <= last[0]; i++ {
+		for j := first[1]; j <= last[1]; j++ {
+			if lights[i][j] == 0 {
+				lights[i][j] = 1
+			} else if lights[i][j] == 1 {
+				lights[i][j] = 0
 			}
 		}
+
 	}
+
 	return lights
 }
 
@@ -76,8 +72,7 @@ func Run_lights() {
 
 	var lights [1000][1000]int
 
-	for i := range lights_instructions[:1] {
-		fmt.Println(lights_instructions[i])
+	for i := range lights_instructions {
 		var edges []int = get_edges(lights_instructions[i])
 
 		if strings.Contains(lights_instructions[i], "toggle") {
@@ -92,13 +87,14 @@ func Run_lights() {
 
 	var light_turned_on int = 0
 
-	for i := range lights {
-		if lights[i][2] == 1 {
-			light_turned_on = light_turned_on + 1
+	for i := range 1000 {
+		for j := range 1000 {
+			if lights[i][j] == 1 {
+				light_turned_on = light_turned_on + 1
+			}
 		}
 	}
 
 	fmt.Println(light_turned_on)
-	fmt.Println(all_lights)
 
 }
