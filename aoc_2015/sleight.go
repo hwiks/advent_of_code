ea5e2f7f53d48cfe2f33d2407fa8ba8b245aa582
Anna Lanteri <annalanterip@gmail.com>
2025-10-13 13:52:13 +0200

    Solves day 8.2

--- aoc_2015/sleight.go
+++ aoc_2015/sleight.go
@@ -11,6 +11,7 @@ func Run_sleight() {
 
 	var str_lit_len int = 0
 	var total_n_characters int = 0
+	var total_with_extra int = 0
 
 	for _, present := range sleight_list {
 
@@ -18,14 +19,16 @@ func Run_sleight() {
 
 		var present_string, err = strconv.Unquote(present)
 		check_error(err)
-		fmt.Println(present_string)
+
+		var present_quoted = strconv.Quote(present)
 
 		total_n_characters += len(present_string)
 
+		total_with_extra += len(present_quoted)
+
 	}
 
-	fmt.Println(str_lit_len)
-	fmt.Println(total_n_characters)
-	fmt.Println(str_lit_len - total_n_characters)
+	fmt.Println("For the first riddle: ", str_lit_len-total_n_characters)
+	fmt.Println("For the second riddle: ", total_with_extra-str_lit_len)
 
 }
