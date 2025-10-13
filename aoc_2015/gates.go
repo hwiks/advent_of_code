73e1a126c0d5956eaca798323d889f01a943a207
Anna Lanteri <annalanterip@gmail.com>
2025-10-13 12:55:32 +0200

    still not solved: Day 7. I tried so hard, and got so far, and in the end, it doesnt even run yet

--- aoc_2015/gates.go
+++ aoc_2015/gates.go
@@ -6,46 +6,182 @@ import (
 	"strings"
 )
 
-func Run_circuits() {
-
-	var circuits_commands []string = extract_lines("inputs/circuits_test.txt")
-
+func initialize_circuit_map(commands []string) map[string]string {
 	var circuits = make(map[string]string, 0)
 
-	for i := range circuits_commands {
+	for i := range commands {
 
-		var circuits_components []string = strings.Split(circuits_commands[i], "->")
+		var circuits_components []string = strings.Split(commands[i], "->")
 
-		circuits[circuits_components[1]] = circuits_components[0][:len(circuits_components[0])-1]
+		var circuit_var string = strings.Replace(circuits_components[1], " ", "", -1)
+		var circuit_comm string = strings.Replace(circuits_components[0], " ", "", -1)
+
+		circuits[circuit_var] = circuit_comm
+
+	}
+	return circuits
+}
+
+func solve_solvable(circuit_map map[string]string) int {
+
+	var solved map[string]string = find_and_execute_solvable(circuit_map)
+
+	fmt.Println("solved:", solved)
+
+	sub_solved(solved, circuit_map)
+	fmt.Println("circuit map after substitution: ", circuit_map)
+
+	return len(solved)
+
+}
+
+func find_and_execute_solvable(circuit_map map[string]string) map[string]string {
+
+	var solvable map[string]uint16 = make(map[string]uint16)
+
+	for key, circuit := range circuit_map {
+		if num_value, err := strconv.Atoi(circuit); err == nil {
+			solvable[key] = uint16(num_value)
+		}
+
+		if is_solvable(circuit) {
+			fmt.Println("It's solvable!", circuit)
+			circuit_map[key] = solve_circuit(circuit)
+			fmt.Println(solvable[key])
+		}
 
 	}
 
-	fmt.Println(circuits)
-	// simplify all simplifiable expressions, then solve for desired variables
-	// this would be my human approach but there is def better ways out there
-	for variable, circuit := range circuits {
-		if num_value, err := strconv.Atoi(circuit); err == nil {
-			fmt.Println("Found an int ", variable, num_value)
+	return circuit_map
 
-			//todo: only save the ints
-			//      then run as a function smtg like below for each of the ints (might need some trimming)
-			// this problem needs to be recursive somehow
-			// I also need to look into the gates: there must be libraries for the gates commands
+}
 
-			for var_nest, circuit_nest := range circuits {
-				if strings.Contains(circuit_nest, variable) {
-					fmt.Println("found the var here ", circuit_nest)
-					var new_circuit = strings.Replace(circuit_nest, variable, " "+strconv.Itoa(num_value), -1)
+func solve_circuit(circuit string) string {
 
-					circuits[var_nest] = new_circuit
+	var result uint16
+	if strings.Contains(circuit, "AND") {
+		var variables []string = strings.Split(circuit, "AND")
+		result1, err1 := strconv.Atoi(variables[0])
+		result2, err2 := strconv.Atoi(variables[1])
 
-				}
+		result = uint16(result1) & uint16(result2)
+		fmt.Println("and result", result)
 
-			}
-			delete(circuits, variable)
-			fmt.Println(circuits)
+		check_error(err1)
+		check_error(err2)
+
+	} else if strings.Contains(circuit, "OR") {
+		var variables []string = strings.Split(circuit, "OR")
+		result1, err1 := strconv.Atoi(variables[0])
+		result2, err2 := strconv.Atoi(variables[1])
+
+		result = uint16(result1) | uint16(result2)
+
+		check_error(err1)
+		check_error(err2)
+
+	} else if strings.Contains(circuit, "RSHIFT") {
+		var variables []string = strings.Split(circuit, "RSHIFT")
+		result1, err1 := strconv.Atoi(variables[0])
+		result2, err2 := strconv.Atoi(variables[1])
+
+		result = uint16(result1) >> uint16(result2)
+
+		check_error(err1)
+		check_error(err2)
+
+	} else if strings.Contains(circuit, "LSHIFT") {
+		var variables []string = strings.Split(circuit, "LSHIFT")
+		result1, err1 := strconv.Atoi(variables[0])
+		result2, err2 := strconv.Atoi(variables[1])
+
+		result = uint16(result1) >> uint16(result2)
+
+		check_error(err1)
+		check_error(err2)
+
+	} else if strings.Contains(circuit, "NOT") {
+		var variables []string = strings.Split(circuit, "NOT")
+		result1, err1 := strconv.Atoi(variables[0])
+
+		result = ^uint16(result1)
+
+		check_error(err1)
+
+	}
+
+	return strconv.Itoa(int(result))
+
+}
+
+func all_ints(circuit string, command string) bool {
+
+	var all_ints bool
+
+	if strings.Contains(circuit, command) {
+		var variables = strings.Split(circuit, command)
+		if is_int(variables[0]) && is_int(variables[1]) {
+			all_ints = true
 		}
 
+	}
+	return all_ints
+}
+
+func is_solvable(circuit string) bool {
+
+	var excluding_not bool = all_ints(circuit, "AND") || all_ints(circuit, "OR") || all_ints(circuit, "RSHIFT") || all_ints(circuit, "LSHIFT")
+	var only_not bool
+
+	if strings.Contains(circuit, "NOT") {
+		var variables = strings.Split(circuit, "NOT")
+		if is_int(variables[0]) {
+			only_not = true
+		}
+
+	}
+
+	return excluding_not || only_not
+}
+
+func is_int(s string) bool {
+	if _, err := strconv.Atoi(s); err == nil {
+		return true
+	} else {
+		return false
+	}
+
+}
+
+func sub_solved(solvable map[string]string, circuit_map map[string]string) {
+
+	for solv_key, solv_value := range solvable {
+		for key, circuit := range circuit_map {
+
+			if strings.Contains(circuit, solv_key) {
+
+				var new_value = solv_value
+				fmt.Println(circuit_map[key])
+				fmt.Println(strings.Replace(circuit, solv_key, new_value, -1))
+
+				circuit_map[key] = strings.Replace(circuit, solv_key, new_value, -1)
+				fmt.Println(circuit_map[key])
+
+			}
+
+		}
+	}
+
+}
+
+func Run_circuits() {
+
+	var circuits_commands []string = extract_lines("inputs/circuits_test.txt")
+	var circuit_map map[string]string = initialize_circuit_map(circuits_commands)
+
+	for {
+		var new_found int = solve_solvable(circuit_map)
+		fmt.Println(new_found)
 	}
 
 }
